86f626e37432817bb3535c9f41d6f2bdce5b7775
stepan <sypachev_s_s@mail.ru>
2018-12-02 23:08:19 +0500

    test any

--- tests/anyTest.js
+++ tests/anyTest.js
@@ -6,6 +6,15 @@ describe("Should test any validator", () => {
 	it ("Should test", () => {
 		let [err, options] = new Validator()
 		.arg('a', ["a", 1, {}]).required.any.build();
+		
+		chai.expect(options.a).to.deep.equal(["a", 1, {}]);
+	});
+	
+	it ("Should use defule any type", () => {
+		let [err, options] = new Validator()
+		.arg('a', ["a", 1, {}]).required.build();
+		
+		chai.expect(options.a).to.deep.equal(["a", 1, {}]);
 	});
 	
 });
