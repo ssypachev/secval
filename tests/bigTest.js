c9a6988eb0006c0ad7b8e8661f6cf6abcb9f3fe0
stepan <sypachev_s_s@mail.ru>
2018-12-08 00:58:53 +0500

    new test

--- tests/bigTest.js
+++ tests/bigTest.js
@@ -43,6 +43,34 @@ describe("Should test array validator", () => {
 		chai.expect(err).to.be.null;
 		chai.expect(options).to.have.property('currency');
 		chai.expect(options.currency).to.equal('RUB');
+		chai.expect(options).not.to.have.property('settings');
+	});
+	
+	it ("Should test empty object with default values", () => {
+		let req = {
+			body: {
+			}
+		};
+		let [err, options] = new Validator().with(req.body)
+			.arg('name').optional.string
+			.arg('currency').optional.trim.string.default('RUB').exactly(3).toUpperCase
+			.arg('address').optional.string
+			.arg('description').optional.string
+			.arg('latitude').optional.float
+			.arg('longitude').optional.float
+			.arg('settings').optional.default({}).object
+				.arg('a').optional.int.default(100)
+			.end
+			.arg('deviceId').optional.string
+			.compound.allOrNothing('latitude', 'longitude').build();
+		
+		chai.expect(err).to.be.null;
+		chai.expect(options).to.have.property('currency');
+		chai.expect(options.currency).to.equal('RUB');
+		chai.expect(options).to.have.property('settings');
+		chai.expect(options.settings.a).to.equal(100);
+		
+		console.log(options);
 	});
 	
 });
