c9950ba5becc1860dc5a535d852b6c426714583e
stepan <sypachev_s_s@mail.ru>
2019-02-28 01:22:49 +0500

    tests

--- tests/phoneTest.js
+++ tests/phoneTest.js
@@ -12,4 +12,23 @@ describe('Should test phone', () => {
 		chai.expect(err).to.be.null;
 		chai.expect(options.a).to.equal('+85265698900');
 	});
+	it('should test valid with country', () => {
+		let arg = {
+			a: '+8 526 569-8900'
+		};
+		let [err, options] = new Validator()
+		.with(arg).arg('a').required.country('HKG').phone.build();
+
+		chai.expect(err).to.be.null;
+		chai.expect(options.a).to.equal('+85265698900');
+	});
+	it('should test valid with invalid country', () => {
+		let arg = {
+			a: '+8 526 569-8900'
+		};
+		let [err, options] = new Validator()
+		.with(arg).arg('a').required.country('USA').phone.build();
+
+		chai.expect(err).not.to.be.null;
+	});
 });
